54d2b130bcb68c3a02b0bb834f9295eec4990ba7
Lennart Becker <lenny@localhost.localdomain>
2018-07-17 16:52:29 +0200

    Liste playlists auf;getplaylisttracks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ interface data {
   items: string[];
   length;
   next;
+  tracks;
 }
 @Component({
   selector: 'app-root',
@@ -19,7 +20,7 @@ export class AppComponent {
   clientid = '0224b8e36c9f4b698876dc79b4b88fe7'
   callback = 'http%3A%2F%2Flocalhost%3A4200%2Fcallback'
   url = `https://accounts.spotify.com/authorize?client_id=` + this.clientid + `&redirect_uri=` + this.callback + `&scope=user-read-private%20user-read-email&response_type=token`
-
+  tracks;
   async startLoading() {
     let test = await this.getPlayLists()
     console.log("danach")
@@ -36,11 +37,17 @@ export class AppComponent {
     this.playlists = playlists.items;
   }
   getTracks(url) {
-    console.log(url)
     let token = this.getAccessToken()
     this.spotify.getTracks(token, url)
-      .subscribe((data) => {
-        console.log(data)
+      .subscribe((data:data) => {
+        let items = data.tracks.items
+        // this.tracks.push({test:"test"})
+        
+        // console.log(data.tracks.items)
+        let test = []
+        this.tracks.push(...data.tracks.items)
+        console.log("test",test)
+        console.log(this.tracks)
       })
   }
 
