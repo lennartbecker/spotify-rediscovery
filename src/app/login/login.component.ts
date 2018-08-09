5d1586431346ffeb0710e4eb849e1a78220981f9
Lenny <lenn@localhost.localdomain>
2018-08-09 13:49:51 +0200

    Polish FilterSettings Mobile Layout

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,7 +11,7 @@ export class LoginComponent implements OnInit {
   clientid = '0224b8e36c9f4b698876dc79b4b88fe7'
   callback = 'http%3A%2F%2Flocalhost%3A4200%2Fcallback'
   url = `https://accounts.spotify.com/authorize?client_id=` + this.clientid + `&redirect_uri=` + this.callback + `&scope=user-read-private%20user-read-email&response_type=token`
- 
+  currentYear = new Date().getFullYear()
   ngOnInit() {
   }
 
